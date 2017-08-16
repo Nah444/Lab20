c021d451a5f2c61529ac490c3a9144cd96f04b8b
Nicholos Hatcher <nicholos.hatcher@gmail.com>
2017-08-16 08:33:32 -0400

    first commit

--- src/main/java/com/guenther/spring2/controller/HomeController.java
+++ src/main/java/com/guenther/spring2/controller/HomeController.java
@@ -5,24 +5,68 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public ModelAndView helloWorld()
-    {
-        ModelAndView mv = new
-                ModelAndView("welcome");
-        mv.addObject("message","Hello World!!!");
-        mv.addObject("title", "Welcome to GC Coffee Shop");
-        return mv;
+    @RequestMapping(value = "Menu")
+    public ModelAndView helloWorld() {
+        // defined the data for the connection
+        //address of the database
+        String dbAddress = "jdbc:mysql://localhost:3306/coffeeshopdb";
+        //user name
+        String username = "root";
+        //password
+        String password = "W0rxt!m3";
+
+        //load the driver, needs to be wrapped in try catch so that program doesn't crash:
+        try {
+            //dynamic:
+            Class.forName("com.mysql.jdbc.Driver");
+            //static load: (commented out for now)
+            //DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+
+            //create the db connection object
+            Connection mysqlConnection = DriverManager.getConnection(dbAddress, username, password);
+            //create the db statement
+            String readCustomersCommand = "select name,description,quantity, price from items";
+            Statement readCustomers = mysqlConnection.createStatement(); // creates the statement
+            ResultSet results = readCustomers.executeQuery(readCustomersCommand);
+            //executes the statement / query to get the data from the database, stores in ResultSet called results
+            //array list of customers
+            ArrayList<Item> inventory = new ArrayList<Item>();
+            //map from the ResultSet to the ArrayList, one row at a time, using results.next()
+            while (results.next()) {
+                //loops to make sure there is a next row in results
+                //depends on type of data you are trying to fetch
+                Item temp = new Item(results.getString(1),
+                        results.getString(2), results.getInt(3), results.getFloat(4));
+                //adds temporary customer object to inventory
+                inventory.add(temp);
+            }
+            return new ModelAndView("Menu", "cList", inventory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // todo: create an error page with custom error messages !!!!
+        return null;
     }
 
     @RequestMapping("/register")
-    public ModelAndView register () {
-        return new ModelAndView("register", "inst", //creating objects of multi strings
-                "Please enter info: "); //model value
+    public ModelAndView register() {
+        return new ModelAndView("register", "inst",
+                "Please enter info: ");
     }
+//    @RequestMapping("/login")
+//    public ModelAndView login ( @RequestParam("firstname") String firstname,
+//                                @RequestParam("lastname") String lastname) {
+//
+//        ModelAndView mv = new ModelAndView("login");
+//
+//        return new ModelAndView("login");
+//    }
 
     //action that gets called>
     @RequestMapping("/formhandler")
@@ -31,10 +75,8 @@ public class HomeController {
             @RequestParam("lastname") String lastname,
             @RequestParam("email") String email,
             @RequestParam("phonenumber") String phonenumber,
-            @RequestParam("password") String password)
-
-    {
-        ModelAndView mv = new ModelAndView("AddUser");
+            @RequestParam("password") String password) {
+        ModelAndView mv = new ModelAndView("adduser");
         mv.addObject("firstname", firstname);
         mv.addObject("lastname", lastname);
         mv.addObject("email", email);
@@ -43,5 +85,4 @@ public class HomeController {
 
         return mv;
     }
-
 }
--- src/main/java/com/guenther/spring2/util/AppConfig.java
+++ src/main/java/com/guenther/spring2/util/AppConfig.java
@@ -1,4 +1,4 @@
-package com.guenther.spring2.util;
+package com.guenther.spring2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +11,28 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
+    //view resolver takes a view name, looks in the folder, and adds .jsp to it
+    //there is a way to do this in XML, but this is the JAVA way to do things
     @Bean
-    public InternalResourceViewResolver viewResolver() {
+    public InternalResourceViewResolver viewResolver()
+    {
         InternalResourceViewResolver r = new InternalResourceViewResolver();
         r.setPrefix("WEB-INF/views/");
         r.setSuffix(".jsp");
         return r;
     }
-
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
-
-   /* @Override
+    //resource handler, when you go to any website, request goes to dispatcher servlet. takes request, and looks for mapping
+    //there's a method defined to handle "/", etc...
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
-    }*/
+        //if someone requests anything in the /resources directory, handle it
+        //anything placed here will be served up from webapp
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(3600);
+    }                                                                                                //^^^^instead of hitting the harddrive
+    // keep these for x amount of time in broswer
+
 }
